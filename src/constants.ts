a0c76d634b27c626cfd2505cd13c1c97fd2f16fa
Dmytro Gordon <g-d@ukr.net>
2023-08-14 16:04:57 +0300

    Fix the tests

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import caninsterIds from "./ic/canister_ids.json";
 export const RPC_URL = process.env.PRC_URL || "http://127.0.0.1:8545";
-export const IC_HOST = process.env.IC_HOST || "http://127.0.0.1:8080/";
+export const IC_HOST = process.env.IC_HOST || "http://127.0.0.1:8000/";
 export const LOCAL_TEST_SEED_PHRASE = process.env.LOCAL_TEST_SEED_PHRASE || "";
 export const DEFAULT_IDENTITY = process.env.PEM_FILE_PATH || "";
 export const TEST_TOKEN_PRINCIPAL = caninsterIds.token.local;
