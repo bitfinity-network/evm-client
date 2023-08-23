1179c62f333812095bf20fe894602c5c89e6492f
leleking <simeonnortey@gmail.com>
2023-08-23 15:53:20 +0000

    EPROD-493 added cache keys

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,8 @@ export const LOCAL_TEST_SEED_PHRASE = process.env.LOCAL_TEST_SEED_PHRASE || "";
 export const DEFAULT_IDENTITY = process.env.PEM_FILE_PATH || "";
 export const TEST_TOKEN_PRINCIPAL = caninsterIds.token.local;
 export const MINTER_CANISTER = caninsterIds.minter.local;
+
+// Cache Keys
+export const CACHE_KEYS = {
+  BURNT_TX: "evm_client_burnt_tx_hash",
+};
